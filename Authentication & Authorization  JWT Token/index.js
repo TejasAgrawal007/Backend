fad25458355ee9284a775ff65d108c205902e8ee
Tejas Agrawal <72118095+TejasAgrawal007@users.noreply.github.com>
2024-05-26 13:49:09 +0530

    Update index.js

--- Authentication & Authorization  JWT Token/index.js
+++ Authentication & Authorization  JWT Token/index.js
@@ -1,81 +1,75 @@
-const path = require('path')
-const bcrypt = require('bcrypt')
-const express = require('express')
-const jwt = require('jsonwebtoken')
-const cookieParser = require('cookie-parser')
-const app = express()
-const port = 3000
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const express = require("express");
+const bcrypt = require("bcrypt");
+const path = require("path");
+const app = express();
+const port = 3000;
 
-const userModel = require('./models/user')
+const userSchema = require("./models/user");
 
-app.use(express.json())
-app.use(express.urlencoded({extended : true}))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
+app.use(cookieParser());
+app.set("view engine", "ejs");
 
-app.use(cookieParser())
-app.set('view engine', 'ejs')
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
+app.post("/create", (req, res) => {
+  let { username, email, password, age } = req.body;
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      const creteUser = await userSchema.create({
+        username,
+        email,
+        password: hash,
+        age,
+      });
 
-/*
-app.post('/create',  (req, res) => {
+      let token = jwt.sign({ email }, "secret");
+      res.cookie("token", token);
 
-    let {username, email, password, age} = req.body; 
-
-    bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(password, salt, async (err, hash) => {
-            const createUser = await userModel.create({
-                username,
-                email,
-                password: hash ,
-                age,
-            })
-
-            let token = jwt.sign({email}, "secret");
-            res.cookie("token" , token)
-            res.send(createUser);
-        })
+      res.send(creteUser);
     });
-})
+  });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", async (req, res) => {
+  let user = await userSchema.findOne({ email: req.body.email });
+  console.log(user);
+  if (!user) return res.send("Something went wrong...");
+
+  // console.log(user.password, req.body.password);
+
+  bcrypt.compare(req.body.password, user.password, (err, result) => {
+    if (result) {
+      let token = jwt.sign({ email : user.email }, "secret");
+      res.cookie("token", token);
+      res.send("Yes You can Log In");
 
 
-app.post('/login', async (req, res) => {
-    res.render("login");
-})
-
-app.post('/login', async (req, res) => {
-    // res.render("login");
-    let user = await userModel.findOne({email : req.body.email})
-    console.log(user);
-})
-
-app.get('/logout', (req, res) => {
-    res.cookie("token", "");
-    res.redirect("/")
-})*/
+    } else {
+      res.send("You can't LogIn");
+    }
 
 
-app.post('/create',  (req, res) => {
-    
-    let {username, email, password, age} = req.body;
+  });
+});
 
-    bcrypt.genSalt(10, salt,   (err, salt) => {
-        bcrypt.hash(password, salt, async (err, salt) => {
-            const createUser = await  userModel.create({
-                username, 
-                email,
-                password,
-                age,
-            })
-            res.send(createUser);
-        })
-    })
-})
+app.get("/logout", (req, res) => {
+  res.cookie("token", "");
+  res.redirect("/")
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port port`)
-})
+  console.log(`Example app listening on port port`);
+});
